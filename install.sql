006cd72cf59f7ab466032f47def961da050d5163
Johannes Donath <webmaster@mail.punksoft.net>
2010-11-14 15:29:56 +0100

    Added language table

--- install.sql
+++ install.sql
@@ -1,1 +1,9 @@
+-- Uncomment the following line to create a default database
 -- CREATE DATABASE `evilcode_services` DEFAULT CHARACTER SET utf8 COLLATE utf8_bin;
+
+-- Language database
+CREATE TABLE `evilcode_services`.`language` (
+	`languageID` INT UNSIGNED NOT NULL AUTO_INCREMENT PRIMARY KEY ,
+	`name` VARCHAR( 255 ) NOT NULL ,
+	`code` VARCHAR( 255 ) NOT NULL
+) ENGINE = MYISAM ;
