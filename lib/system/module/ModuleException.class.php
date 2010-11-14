df784ad4a44cb58300fb67ca133f3eb25f267f78
Johannes Donath <webmaster@mail.punksoft.net>
2010-11-14 06:27:58 +0100

    Implemented custom exception ModuleException

---
+++ lib/system/module/ModuleException.class.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Defines a special exception that can thrown in ModuleManager and modules
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ */
+class ModuleException extends Exception { }
+?>
