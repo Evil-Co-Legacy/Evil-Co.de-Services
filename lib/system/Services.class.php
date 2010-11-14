d6dd279718bcc5db42e5d0fd66322af414e0cdab
Johannes Donath <webmaster@mail.punksoft.net>
2010-11-14 10:31:39 +0100

    Added special method calls and implemented autoload method

--- lib/system/Services.class.php
+++ lib/system/Services.class.php
@@ -1,4 +1,21 @@
 <?php
+// define exception handler
+set_exception_handler(array('Services', 'handleException'));
+
+// define error handler
+set_error_handler(array('Services', 'handleError'), E_ALL);
+
+/**
+ * Autoloads default classes
+ * @param	string	$className
+ */
+function __autoload($className) {
+	// autoload utils
+	if (file_exists(SDIR.'lib/utils/'.$className.'.class.php')) {
+		require_once(SDIR.'lib/utils/'.$className.'.class.php');
+		return;
+	}
+}
 
 /**
  * Manages all needed core instances
