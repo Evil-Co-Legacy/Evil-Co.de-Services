d9af591b2935b0af55f115fc219b4774b8676fb8
Johannes Donath <webmaster@mail.punksoft.net>
2010-11-14 11:29:02 +0100

    Implemented Bot

---
+++ lib/system/user/Bot.class.php
@@ -0,0 +1,13 @@
+<?php
+// imports
+require_once(SDIR.'lib/system/user/AbstractUserType.class.php');
+
+/**
+ * Represents a bot user
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ */
+class Bot extends AbstractUserType {
+	// Nothing to do here
+}
+?>
