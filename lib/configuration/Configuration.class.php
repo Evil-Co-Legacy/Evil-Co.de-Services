69c30525d38459b7d02374f176768566d6df549f
Johannes Donath <webmaster@mail.punksoft.net>
2010-11-14 11:43:59 +0100

    Implemented Configuration

---
+++ lib/configuration/Configuration.class.php
@@ -0,0 +1,63 @@
+<?php
+
+/**
+ * Manages the XML configuration
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ */
+class Configuration {
+	
+	/**
+	 * Contains all configuration variables
+	 * @var	array
+	 */
+	protected $configuration = array();
+	
+	/**
+	 * Creates a new instance of type Configuration
+	 */
+	public function __construct() {
+		if (!file_exists(SDIR.'config/config.xml')) throw new Exception("Startup error: config.xml not found in ".SDIR."config/! Aborted start!");
+		
+		// load xml
+		$xml = new XML(SDIR.'config/config.xml');
+		
+		// get element array
+		$data = $xml->getElementTree('config');
+		
+		// call loop
+		$this->readConfiguration(&$this->configuration, $data);
+	}
+	
+	/**
+	 * Loops through array
+	 * @param	array	$array
+	 * @param	array	$data
+	 */
+	protected function readConfiguration(&$array, $data) {
+		// start loop
+		foreach($data['children'] as $key => $child) {
+			// handle elements with children
+			if (!isset($child['cdata'])) {
+				if (isset($child['children'])) {
+					$array[$child['name']] = array();
+					$this->readConfiguration(&$array[$child['name']]);
+				}
+				
+				continue;
+			}
+			
+			$array[$child['name']] = $child['cdata'];
+		}
+	}
+	
+	/**
+	 * Returnes a configuration variable
+	 * @param	string	$name
+	 */
+	public function get($name) {
+		if (isset($this->configuration[$name])) return $this->configuration[$name];
+		return null;
+	}
+}
+?>
