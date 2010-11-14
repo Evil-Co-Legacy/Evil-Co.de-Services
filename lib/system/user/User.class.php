f54215d46c5eaa7cf922dd545613d208b7819574
Johannes Donath <webmaster@mail.punksoft.net>
2010-11-14 13:57:21 +0100

    Implemented User

---
+++ lib/system/user/User.class.php
@@ -0,0 +1,13 @@
+<?php
+// imports
+require_once(SDIR.'lib/system/user/AbstractUserType.class.php');
+
+/**
+ * Represents an irc user
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ */
+class User extends AbstractUserType {
+	// Nothing to do here
+}
+?>
