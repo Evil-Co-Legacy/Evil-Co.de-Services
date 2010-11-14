79600030efb0c53c9fb9e8f408ecf35c77b3f143
Johannes Donath <webmaster@mail.punksoft.net>
2010-11-14 10:49:13 +0100

    Added code for pidfile

--- start.php
+++ start.php
@@ -2,6 +2,9 @@
 // defines
 define('SDIR', dirname(__FILE__).'/');
 
+// write pidfile
+file_put_contents("services.pid", getmypid());
+
 // imports
 require_once(SDIR.'lib/system/Services.class.php');
 
