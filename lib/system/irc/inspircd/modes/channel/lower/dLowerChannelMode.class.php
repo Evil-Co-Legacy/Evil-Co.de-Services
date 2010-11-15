b6beaad08f3b6979e759b7ecaf8cfa74f8cc5969
Johannes Donath <webmaster@mail.punksoft.net>
2010-11-15 06:43:17 +0100

    Implemented all channel modes listed in /helpop

---
+++ lib/system/irc/inspircd/modes/channel/lower/dLowerChannelMode.class.php
@@ -0,0 +1,17 @@
+<?php
+// imports
+require_once(SDIR.'lib/system/irc/AbstractMode.class.php');
+
+/**
+ * Represents a mode
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ */
+class dLowerChannelMode extends AbstractMode {
+	
+	/**
+	 * @see	AbstractMode::$canHaveArgument
+	 */
+	protected static $canHaveArgument = true;
+}
+?>
---
+++ lib/system/irc/inspircd/modes/channel/lower/zLowerChannelMode.class.php
@@ -0,0 +1,13 @@
+<?php
+// imports
+require_once(SDIR.'lib/system/irc/AbstractMode.class.php');
+
+/**
+ * Represents a mode (What's this?!)
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ */
+class zLowerChannelMode extends AbstractMode {
+	// Nothing to do here
+}
+?>
