c7be50d44e2db0f681cf9e6e8a9e6e941ac1e09f
Johannes Donath <webmaster@mail.punksoft.net>
2010-11-16 01:58:48 +0100

    Rewritten module system (We're now using runkit to allow reload/unload)

--- lib/system/module/ModuleManager.class.php
+++ lib/system/module/ModuleManager.class.php
@@ -10,21 +10,19 @@ require_once(SDIR.'lib/system/module/ModuleException.class.php');
 class ModuleManager {
 	
 	/**
-	 * Contains all modules
+	 * Contains all availablemodules
 	 * @var	array<Module>
 	 */
-	protected $moduleList = array();
-	
-	/**
-	 * Contains all runtime files
-	 * @var array<string>
-	 */
-	protected $runtimeFiles = array();
+	protected $availableModules = array();
 	
 	/**
 	 * Creates a new instance of ModuleManager
 	 */
 	public function __construct() {
+		// check dependencies
+		if (!extension_loaded('runkit')) throw new ModuleException("Required runtime libary not found!");
+		
+		// load modules from database
 		$sql = "SELECT
 					*
 				FROM
@@ -45,61 +43,15 @@ class ModuleManager {
 		if (!file_exists($file)) throw new ModuleException("Cannot find module file '".$file."'");
 		if (!is_readable($file)) throw new ModuleException("Cannot read module file '".$file."'");
 		
-		// read module file
-		$moduleFile = file_get_contents($file);
-		
 		// get class name
-		$className = basename($file, '.class.php');
+		$moduleName = basename($file, '.class.php');
 		
-		// create runtime name
-		$runtimeName = sha1($className.time());
-		
-		// modify
-		$moduleFile = preg_replace('~^class (.*)(| implements (.*)) extends (.*)~', 'class '.$runtimeName.' $2 $4');
-		
-		// register runtime file
-		$this->registerRuntimeFile($runtimeName.'.php');
-		
-		// create file
-		$this->createRuntimeFile($runtimeName.'.php', $moduleFile);
-		
-		// include runtime file
-		require_once($this->getRuntimeFilePath($runtimeName.'.php'));
-		
-		// call special module methods
-		switch(get_parent_class($this->moduleList[$index])) {
-			case 'Bot':
-				call_user_func(array($runtimeName, 'registerBot'));
-				break;
-			case 'Command':
-				call_user_func(array($runtimeName, 'registerCommand'));
-				break;
-		}
-	}
-	
-	/**
-	 * Registeres a new runtime file
-	 * @param	string	$filename
-	 */
-	public function registerRuntimeFile($filename) {
-		$this->runtimeFiles[] = $filename;
-	}
-	
-	/**
-	 * Creates a new runtime file
-	 * @param	string	$filename
-	 * @param	string	$content
-	 */
-	public function createRuntimeFile($filename, $content) {
-		file_put_contents(SDIR.'runtime/'.$filename, $content);
-	}
-	
-	/**
-	 * Returnes the absolute path to runtime file
-	 * @param	string	$filename
-	 */
-	public function getRuntimeFilePath($filename) {
-		return SDIR.'runtime/'.$filename;
+		// create runkit sandbox
+		$this->availableModules[$moduleName] = array('sandbox' => new Runkit_Sandbox(Services::getConfiguration()->get('modules')));
+		$this->availableModules[$moduleName]['sandbox']->eval("require_once('".$file."');");
+		$this->availableModules[$moduleName]['sandbox']->eval('$moduleType = (is_subclass_of("BotModule") ? "Bot" : (is_subclass_of("CommandModule") ? "Command" : "Extension"));');
+		$this->availableModules[$moduleName]['type'] = $this->availableModules[$moduleName]['sandbox']->moduleType;
+		$this->availableModules[$moduleName]['sandbox']->eval('unset($moduleType);');
 	}
 }
 ?>
