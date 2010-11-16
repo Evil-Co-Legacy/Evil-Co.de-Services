ebd8a6c77a9cc2c52523ffd620033430e8a8dc1a
Johannes Donath <webmaster@mail.punksoft.net>
2010-11-16 02:02:57 +0100

    Added module type 'Extension'

---
+++ lib/modules/ExtensionModule.class.php
@@ -0,0 +1,26 @@
+<?php
+// imports
+require_once(SDIR.'lib/modules/Module.class.php');
+
+/**
+ * Defines default methods for extensions
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ */
+abstract class ExtensionModule extends Module {
+	
+	/**
+	 * Creates a new instance of type Command
+	 * @param	BotModule	$bot
+	 * @param	string		$name
+	 */
+	public function __construct() {
+		$this->registerEvents();
+	}
+	
+	/**
+	 * @see	Module::registerEvents()
+	 */
+	public function registerEvents();
+}
+?>
