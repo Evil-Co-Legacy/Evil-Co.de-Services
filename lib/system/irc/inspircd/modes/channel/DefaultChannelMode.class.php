96d4b1b7a91937d6444cf441b04e1cbdfaf65491
Johannes Donath <akkarin@develfusion.com>
2010-12-26 10:57:41 +0100

    Added DefaultChannelMode

---
+++ lib/system/irc/inspircd/modes/channel/DefaultChannelMode.class.php
@@ -0,0 +1,14 @@
+<?php
+// imports
+require_once(SDIR.'lib/system/irc/AbstractMode.class.php');
+
+/**
+ * Represents a mode
+ * Note: This is just a fallback to prevent crashes
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ */
+class DefaultChannelMode extends AbstractMode {
+	// Nothing to do here
+}
+?>
