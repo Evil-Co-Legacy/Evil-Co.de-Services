61c050052f1dfa79de121802faa3958d0df5ca15
Johannes Donath <akkarin@develfusion.com>
2010-12-26 13:18:40 +0100

    Added hardcoded languageID ...

--- lib/system/language/LanguageManager.class.php
+++ lib/system/language/LanguageManager.class.php
@@ -6,19 +6,19 @@
  * @copyright	2010 DEVel Fusion
  */
 class LanguageManager {
-	
+
 	/**
-	 * Contains all 
+	 * Contains all
 	 * @var unknown_type
 	 */
 	protected $availableLanguages = array();
-	
+
 	/**
 	 * Contains all items
 	 * @var	array
 	 */
 	protected $items = array();
-	
+
 	/**
 	 * Creates a new instance of LanguageManager
 	 */
@@ -28,41 +28,44 @@ class LanguageManager {
 				FROM
 					language";
 		$result = Services::getDB()->sendQuery($sql);
-		
+
 		while($row = Services::getDB()->fetchArray($result)) {
 			$this->availableLanguages[] = $row;
 			$this->items[$row['languageID']] = array();
 		}
-		
+
 		$sql = "SELECT
 					*
 				FROM
 					language_item";
 		$result = Services::getDB()->sendQuery($sql);
-		
+
 		while($row = Services::getDB()->fetchArray($result)) {
 			$this->items[$row['languageID']][$row['name']] = $row['value'];
 		}
 	}
-	
+
 	/**
 	 * Returnes the content of the given language var (This method returnes the name of the variable if no matching variable exists)
 	 * @param	integer	$languageID
 	 * @param	string	$variable
 	 */
 	public function get($languageID, $variable) {
+		// whohoo hardcoded shit
+		if (!$languageID == null) $languageID = 1;
+
 		// handle missing vars
 		if (!isset($this->items[$languageID][$variable])) return $variable;
-		
+
 		// create needed vars
 		$value = $this->items[$languageID][$variable];
 		$arguments = func_get_args();
-		
+
 		// kick languageID and variable from argument list
 		unset($arguments[0]);
 		unset($arguments[1]);
 		$arguments = array_merge(array(), $arguments);
-		
+
 		// return correct value
 		return call_user_func_array('sprintf', $arguments);
 	}
