1e5fc40df4a7ee1af7b7f3d907ae858698a5a36d
Johannes Donath <akkarin@develfusion.com>
2010-12-26 12:34:58 +0100

    We should send notices

--- lib/system/user/Bot.class.php
+++ lib/system/user/Bot.class.php
@@ -8,11 +8,20 @@ require_once(SDIR.'lib/system/user/AbstractUserType.class.php');
  * @copyright	2010 DEVel Fusion
  */
 class Bot extends AbstractUserType {
-	
+
 	/**
 	 * Markes this UserType as bot
 	 * @var	boolean
 	 */
 	public $isBot = true;
+
+	/**
+	 * Sends a message to $target
+	 * @param	string	$target
+	 * @param	string	$message
+	 */
+	public function sendMessage($target, $message) {
+		Services::getConnection()->getProtocol()->sendNotice($this->getUuid(), $target, $message);
+	}
 }
 ?>
