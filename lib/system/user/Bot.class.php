f18a93e4fba1b20aa823d3b28b85003afa13808f
Johannes Donath <akkarin@develfusion.com>
2010-12-26 20:53:21 +0100

    Added join method (This does not work correctly yet)

--- lib/system/user/Bot.class.php
+++ lib/system/user/Bot.class.php
@@ -23,5 +23,26 @@ class Bot extends AbstractUserType {
 	public function sendMessage($target, $message) {
 		Services::getConnection()->getProtocol()->sendNotice($this->getUuid(), $target, $message);
 	}
+
+	/**
+	 * Joins the bot to given channel
+	 * @param	string	$channel
+	 * @param	string	$modes
+	 * @param	string	$userModes
+	 */
+	public function join($channel, $modes = "+nt", $userModes = "+oaq") {
+		// remove + from string
+		if (stripos($userModes, '+') !== false) $userModes = substr($userModes, 1);
+
+		// join channel
+		Services::getConnection()->getProtocol()->join($this->getUuid(), $channel, $modes, $userModes);
+
+		// notify channel manager
+		if (($chan = Services::getChannelManager()->getChannel($channel)) === null) {
+			Services::getChannelManager()->addChannel($channel, time(), $modes, array(array('mode' => $userModes, 'user' => $this)));
+		} else {
+			$chan->join(array(array('mode' => $userModes, 'user' => $this)));
+		}
+	}
 }
 ?>
