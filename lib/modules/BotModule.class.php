ec62433c00285d864e669460db269c4c882330f9
Johannes Donath <akkarin@develfusion.com>
2010-12-26 11:27:03 +0100

    Added __call method

--- lib/modules/BotModule.class.php
+++ lib/modules/BotModule.class.php
@@ -8,31 +8,31 @@ require_once(SDIR.'lib/modules/Module.class.php');
  * @copyright	2010 DEVel Fusion
  */
 abstract class BotModule implements Module {
-	
+
 	/**
 	 * Contains the name of this bot (This must defined!)
 	 * @var	string
 	 */
 	protected $botName = 'Bot';
-	
+
 	/**
 	 * Contains the user type object of this bot
 	 * @var	UserType
 	 */
 	protected $bot = null;
-	
+
 	/**
 	 * Contains all bound commands for this bot
 	 * @var	array<CommandModule>
 	 */
 	protected $commands = array();
-	
+
 	/**
 	 * Contains the trigger of this bot (The trigger is used for public channel commands)
 	 * @var unknown_type
 	 */
 	protected $trigger = '';
-	
+
 	/**
 	 * Creates a new instance of type Bot
 	 * @param	UserType	$bot
@@ -41,18 +41,18 @@ abstract class BotModule implements Module {
 		$this->bot = $bot;
 		$this->trigger = $trigger;
 		$this->registerEvents();
-		
+
 		// join channel
 		Services::getConnection()->getProtocol()->join($bot->getUuid(), Services::getConnection()->getProtocol()->getServiceChannel());
 	}
-	
+
 	/**
 	 * Returnes the bot reference
 	 */
 	public function getBot() {
 		return $this->bot;
 	}
-	
+
 	/**
 	 * Handles a line
 	 * @param	string	$message
@@ -60,19 +60,19 @@ abstract class BotModule implements Module {
 	 */
 	public final function handleLine($user, $target, $message) {
 		$found = false;
-		
+
 		foreach($this->commands as $key => $command) {
 			if ($this->commands[$key]->matches($message)) {
 				$this->commands[$key]->execute($user, $target, $message);
 				$found = true;
 			}
 		}
-		
+
 		if (!$found) {
 			$this->sendMessage($user, LanguageManager::get($user->languageID, 'bot.global.noSuchCommand'));
 		}
 	}
-	
+
 	/**
 	 * Adds a new command to bot
 	 * @param	CommandModule	$command
@@ -80,19 +80,31 @@ abstract class BotModule implements Module {
 	public function registerCommand($command) {
 		$this->commands[] = $command;
 	}
-	
+
 	/**
 	 * @see	Module::registerEvents()
 	 */
 	public function registerEvents() {
 		// nothing to do here
 	}
-	
+
 	/**
 	 * Registeres a bot (Sas the module manager that this module is available)
 	 */
 	public static function registerBot() {
 		// TODO: Implement this function
+	}
+
+	/**
+	 * Redirects all unknown method calls to user object
+	 * @param	string	$method
+	 * @throws Exception
+	 */
+	public final function __call($method, $arguments) {
+		if (method_exists($this->bot, $method))
+			return call_user_func_array(array($this->bot, $method), $arguments);
+
+		throw new Exception("Method '".$method."' does not exist in class ".get_class($this));
 	}
 }
 ?>
