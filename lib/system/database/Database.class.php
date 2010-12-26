8dd038b0b1ee16434b20e7a8339ffb26df47b013
TimWolla <timwolla@arcor.de>
2010-12-27 00:41:05 +0800

    Corrected identation

--- lib/system/database/Database.class.php
+++ lib/system/database/Database.class.php
@@ -6,58 +6,57 @@
  * @copyright	2010 DEVel Fusion
  */
 abstract class Database {
-    protected $linkID = 0;
-    protected $queryCount = 0;
-    protected $lastResult = null;
-    protected $lastQuery = "";
+	protected $linkID = 0;
+	protected $queryCount = 0;
+	protected $lastResult = null;
+	protected $lastQuery = "";
 
-    /**
-     * Returnes the first row
-     * @param	    string	$query
-     * @param	    integer	$limit
-     * @param	    integer	$offset
-     * @return	    mixed
-     */
-    public function getFirstRow($query, $limit = 1, $offset = 0) {
-        $limit = (preg_match('/LIMIT\s+\d/i', $query) ? 0 : $limit);
+	/**
+	 * Returnes the first row
+	 * @param	    string	$query
+	 * @param	    integer	$limit
+	 * @param	    integer	$offset
+	 * @return	    mixed
+	 */
+	public function getFirstRow($query, $limit = 1, $offset = 0) {
+		$limit = (preg_match('/LIMIT\s+\d/i', $query) ? 0 : $limit);
 
-        $query = $this->handleLimitParameter($query, $limit, $offset);
+		$query = $this->handleLimitParameter($query, $limit, $offset);
 
-        $result = $this->sendQuery($query);
+		$result = $this->sendQuery($query);
 
-	if (is_resource($result)) {
+		if (is_resource($result)) {
+			$row = $this->fetchArray($result);
 
-	    $row = $this->fetchArray($result);
+			if (is_array($row)) {
+				return $row;
+			}
+		}
 
-	    if (is_array($row)) {
-		return $row;
-	    }
+		return false;
 	}
 
-	return false;
-    }
+	/**
+	 * Returns a query with limit and offset
+	 * @param string $query
+	 * @param integer $limit
+	 * @param integer $offset
+	 * @return string
+	 */
+	public function handleLimitParameter($query = '', $limit = 0, $offset = 0) {
+		if ($limit != 0) {
+			if ($offset > 0) $query .= ' LIMIT '.$offset.', '.$limit;
+			else $query .= ' LIMIT '.$limit;
+		}
 
-    /**
-     * Returns a query with limit and offset
-     * @param string $query
-     * @param integer $limit
-     * @param integer $offset
-     * @return string
-     */
-    public function handleLimitParameter($query = '', $limit = 0, $offset = 0) {
-        if ($limit != 0) {
-            if ($offset > 0) $query .= ' LIMIT '.$offset.', '.$limit;
-            else $query .= ' LIMIT '.$limit;
-        }
+		return $query;
+	}
 
-        return $query;
-    }
-
-    /**
-     * Deconnects from the database
-     */
-    public function shutdown() {
-	// empty
-    }
+	/**
+	 * Deconnects from the database
+	 */
+	public function shutdown() {
+		// empty
+	}
 }
 ?>
