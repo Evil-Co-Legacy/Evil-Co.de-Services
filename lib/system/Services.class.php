733110ac0ae077b097d8a58c9918b9065f1e54f6
Johannes Donath <akkarin@develfusion.com>
2010-12-26 10:53:04 +0100

    We should check memcache configuration before loading it ...

--- lib/system/Services.class.php
+++ lib/system/Services.class.php
@@ -23,67 +23,67 @@ require_once(SDIR.'lib/system/cache/CacheSource.class.php');
  * @copyright	2010 DEVel Fusion
  */
 class Services {
-	
+
 	/**
 	 * Contains the Configuration object
 	 * @var Configuration
 	 */
 	protected static $configObj = null;
-	
+
 	/**
 	 * Contains the EventHandler object
 	 * @var	EventHandler
 	 */
 	protected static $eventObj = null;
-	
+
 	/**
 	 * Contains the database connection
 	 * @var	Database
 	 */
 	protected static $dbObj = null;
-	
+
 	/**
 	 * Contains the LanguageManager object
 	 * @var	LanguageManager
 	 */
 	protected static $languageObj = null;
-	
+
 	/**
 	 * Contains the IRCConnection object
 	 * @var	IRCConnection
 	 */
 	protected static $ircObj = null;
-	
+
 	/**
 	 * Contains the UserManager object
 	 * @var UserManager
 	 */
 	protected static $userManagerObj = null;
-	
+
 	/**
 	 * Contains the BotManager object
 	 * @var	BotManager
 	 */
 	protected static $botManagerObj = null;
-	
+
 	/**
 	 * Contains the ChannelManager object
 	 * @var	ChannelManager
 	 */
 	protected static $channelManagerObj = null;
-	
+
 	/**
 	 * Contains the ModuleManager object
 	 * @var	ModuleManager
 	 */
 	protected static $moduleManagerObj = null;
-	
+
 	/**
 	 * Contains the current connection to Memcache
 	 * @var	Memcache
 	 */
 	protected static $memcacheObj = null;
-	
+
 	/**
 	 * Creates a new instance of Services
 	 */
@@ -99,163 +99,163 @@ class Services {
 		$this->initConnection();
 		self::$ircObj->start();
 	}
-	
+
 	/**
 	 * Shuts down our services
 	 */
 	public static function destruct() {
 		// call connection shutdown method
 		if (self::getConnection() !== null) self::getConnection()->shutdown();
-		
+
 		// flsuh cache
 		if (self::memcacheLoaded()) self::getMemcache()->flush();
-		
+
 		// remove pidfile (if any)
 		if (file_exists(SDIR.'services.pid')) @unlink(SDIR.'services.pid');
 	}
-	
+
 	/**
 	 * Creates a new configuration object
 	 */
 	protected function initConfiguration() {
 		self::$configObj = new Configuration();
 	}
-	
+
 	/**
 	 * Creates a new EventHandler object
 	 */
 	protected function initEvents() {
 		self::$eventObj = new EventHandler();
 	}
-	
+
 	/**
 	 * Creates a new database connection
 	 */
 	protected function initDB() {
 		// get configuration
 		$db = self::getConfiguration()->get('database');
-		
+
 		// validate
 		if (!isset($db['driver'], $db['hostname'], $db['username'], $db['password'], $db['dbname'])) throw new Exception("Invalid Database configuration!");
-		
+
 		// try to find database driver
 		if (!file_exists(SDIR.'lib/system/database/'.$db['driver'].'Database.class.php')) throw new Exception("Invalid database driver: ".$db['driver']);
-		
+
 		// get drivers classname
 		$className = $db['driver'].'Database';
-		
+
 		// include driver
 		require_once(SDIR.'lib/system/database/'.$className.'.class.php');
-		
+
 		// create new instance
 		self::$dbObj = new $className($db['dbname'], $db['username'], $db['password'], $db['hostname'], 'UTF8');
 	}
-	
+
 	/**
 	 * Creates a new LanguageManager instance
 	 */
 	protected function initLanguage() {
 		self::$languageObj = new LanguageManager();
 	}
-	
+
 	/**
 	 * Creates a new IRCConnection instance
 	 */
 	protected function initConnection() {
 		self::$ircObj = new IRCConnection();
 	}
-	
+
 	/**
 	 * Creates a new UserManager instance
 	 */
 	protected function initUserManager() {
 		self::$userManagerObj = new UserManager();
 	}
-	
+
 	/**
 	 * Creates a new BotManager instance
 	 */
 	protected function initBotManager() {
 		self::$botManagerObj = new BotManager();
 	}
-	
+
 	/**
 	 * Creates an new ChannelManager instance
 	 */
 	protected function initChannelManager() {
 		self::$channelManagerObj = new ChannelManager();
 	}
-	
+
 	/**
 	 * Creates a new ModuleManager instance
 	 */
 	protected function initModules() {
 		self::$moduleManagerObj = new ModuleManager();
 	}
-	
+
 	/**
 	 * Returnes the current configuration object
 	 */
 	public static function getConfiguration() {
 		return self::$configObj;
 	}
-	
+
 	/**
 	 * Returnes the current EventHandler object
 	 */
 	public static function getEvent() {
 		return self::$eventObj;
 	}
-	
+
 	/**
 	 * Returnes the current database connection
 	 */
 	public static function getDB() {
 		return self::$dbObj;
 	}
-	
+
 	/**
 	 * Returnes the current language manager
 	 */
 	public static function getLanguage() {
 		return self::$languageObj;
 	}
-	
+
 	/**
 	 * Returnes the current irc connection
 	 */
 	public static function getConnection() {
 		return self::$ircObj;
 	}
-	
+
 	/**
 	 * Returnes the current user manager object
 	 */
 	public static function getUserManager() {
 		return self::$userManagerObj;
 	}
-	
+
 	/**
 	 * Returnes the current bot manager object
 	 */
 	public static function getBotManager() {
 		return self::$botManagerObj;
 	}
-	
+
 	/**
 	 * Returnes the current channel manager object
 	 */
 	public static function getChannelManager() {
 		return self::$channelManagerObj;
 	}
-	
+
 	/**
 	 * Returnes the current ModuleManager object
 	 */
 	public static function getModuleManager() {
 		return self::$moduleManagerObj;
 	}
-	
+
 	/**
 	 * Loads the memcache extension
 	 */
@@ -263,34 +263,34 @@ class Services {
 		$memcache = Services::getConfiguration()->get('memcache');
 		if ($memcache === null or !extension_loaded('memcache')) {
 			self::getConnection()->getProtocol()->sendLogLine("Cannot use memcache! No configuration found!");
-		} else {
+		} elseif (is_array($memcache['servers'])) {
 			self::$memcacheObj = new CacheSource();
-			
+
 			// add server
 			foreach($memcache['servers'] as $server) {
 				// create needed array
 				$arguments = array();
-				
+
 				// get server address
 				if (!isset($server['address'])) throw new Exception("Invalid memcache configuration: address tag is missing!");
 				$arguments[] = $server['address'];
-				
+
 				// get port
 				if (isset($server['port'])) $arguments[] = $server['port'];
-				
+
 				// get persistent option
 				if (isset($server['persistent'])) $arguments[] = (bool) intval($server['persistent']);
-				
+
 				// add server
 				call_user_func_array(array(self::$memcacheObj, 'addServer'), $arguments);
-				
+
 				// remove temp variables
 				unset($arguments);
 			}
-			
+
 			// flush old cache (we'll have old resources if the applications was cilled)
 			self::$memcacheObj->flush();
-			
+
 			// test connection
 			self::$memcacheObj->add('SERVICES_VERSION', SERVICES_VERSION);
 			if (self::$memcacheObj->get('SERVICES_VERSION') !== false) {
@@ -299,27 +299,27 @@ class Services {
 			} else {
 				// add log entry
 				self::getConnection()->getProtocol()->sendLogLine("An error occoured with memcache! I'll disable it ...");
-				
+
 				// disable memcache
 				self::$memcacheObj = null;
 			}
 		}
 	}
-	
+
 	/**
 	 * Returnes true if memcache is available
 	 */
 	public static function memcacheLoaded() {
 		return (self::$memcacheObj instanceof Memcache);
 	}
-	
+
 	/**
 	 * Returnes the current memcache instance
 	 */
 	public static function getMemcache() {
 		return self::$memcacheObj;
 	}
-	
+
 	/**
 	 * Handles uncought exceptions
 	 * @param	Exception	$ex
@@ -328,11 +328,11 @@ class Services {
 		print("\n\n\n");
 		print($ex);
 		print("\n\n");
-		
+
 		// call connection shutdown method
 		if (self::getConnection() !== null and self::getConnection()->getProtocol() !== null) self::getConnection()->getProtocol()->shutdownConnection($ex->getMessage());
 	}
-	
+
 	/**
 	 * Handles errors
 	 * @param	integer	$errNo
