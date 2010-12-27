bdcc3ae8a94de0bcb021213189c444a25b00e10e
TimWolla <timwolla@arcor.de>
2010-12-27 13:16:02 +0100

    Corrected classname

--- lib/modules/CommandShutdown.class.php
+++ lib/modules/CommandShutdown.class.php
@@ -7,7 +7,7 @@ require_once(SDIR.'lib/modules/CommandModule.class.php');
  * @author		Johannes Donath
  * @copyright	2010 DEVel Fusion
  */
-class CommandLoadModule extends CommandModule {
+class CommandShutdown extends CommandModule {
 
 	/**
 	 * @see CommandModule::$originalName
