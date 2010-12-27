f10098295f80fad73fdfb6e74dc208ca39dd8b3c
Johannes Donath <akkarin@develfusion.com>
2010-12-27 01:03:56 +0100

    Added appearInHelp variable

--- lib/modules/CommandModule.class.php
+++ lib/modules/CommandModule.class.php
@@ -8,38 +8,45 @@ require_once(SDIR.'lib/modules/Module.class.php');
  * @copyright	2010 DEVel Fusion
  */
 abstract class CommandModule implements Module {
-	
+
 	/**
 	 * Contains the bot instance
 	 * @var	BotModule
 	 */
 	protected $bot = null;
-	
+
 	/**
 	 * Contains the name of bound command
 	 * Note: This must be uppercase!
 	 * @var	string
 	 */
 	public $commandName = '';
-	
+
+	/**
+	 * If this is set to false we'll hide the command in HELP command
+	 * @var boolean
+	 */
+	public $appearInHelp = true;
+
 	/**
 	 * Creates a new instance of type Command
 	 * @param	BotModule	$bot
 	 * @param	string		$name
 	 */
-	public function __construct(&$bot, $name) {
+	public function __construct(&$bot, $name, $appearInHelp = true) {
 		$this->commandName = strtoupper($name);
 		$this->bot = &$bot;
+		$this->appearInHelp = $appearInHelp;
 		$this->registerEvents();
 	}
-	
+
 	/**
 	 * Executes the command
 	 * @param	UserObject	$user
 	 * @param	string		$message
 	 */
 	abstract public function execute($user, $target, $message);
-	
+
 	/**
 	 * Returnes true if the given line matches command
 	 * @param	string	$input
@@ -47,19 +54,19 @@ abstract class CommandModule implements Module {
 	 */
 	public final function matches($command) {
 		if (!empty($this->commandName) and strtoupper($command) == $this->commandName) return true;
-		
+
 		// TODO: Add more match types here
-		
+
 		return false;
 	}
-	
+
 	/**
 	 * @see	Module::registerEvents()
 	 */
 	public function registerEvents() {
 		// nothing to do here
 	}
-	
+
 	/**
 	 * Registers the command
 	 */
