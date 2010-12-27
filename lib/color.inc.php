1bcd9159d81ffe57380037302d0b1c88e3988d0e
Johannes Donath <akkarin@develfusion.com>
2010-12-27 18:47:21 +0100

    Removed italic color code (I think not all clients will display this correctly)

--- lib/color.inc.php
+++ lib/color.inc.php
@@ -1,7 +1,6 @@
 <?php
 define('COLOR_BOLD', chr(2));
 define('COLOR_UNDERLINE', chr(31));
-define('COLOR_ITALIC', chr(26));
 define('COLOR_CTCP', chr(1));
 define('COLOR_ACTION', COLOR_CTCP."ACTION");
 ?>
