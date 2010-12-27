4e641e2042d2969850a03b16512f8aa730c7473b
Johannes Donath <akkarin@develfusion.com>
2010-12-27 10:43:47 +0100

    We should prefix all module addresses with 'Cache' (We aren't allowed to use numbers at start of className)

--- lib/system/module/ModuleManager.class.php
+++ lib/system/module/ModuleManager.class.php
@@ -140,7 +140,7 @@ class ModuleManager {
 
 		// get class name
 		$moduleName = basename($file, '.class.php');
-		if (!$moduleAddress === null) $moduleAddress = strtoupper(dechex((time() + count($this->availableModules)) * 100000));
+		if (!$moduleAddress === null) $moduleAddress = "Cache".strtoupper(dechex((time() + count($this->availableModules)) * 100000));
 
 		// validate module
 		if (isset($this->availableModules[$moduleName])) throw new ModuleException("Module '".$moduleName."' is already loaded!");
