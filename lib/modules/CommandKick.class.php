cf99c4d49271aecc7452287aa0d4636083c13276
TimWolla <timwolla@arcor.de>
2010-12-27 22:40:52 +0100

    Permissiondenied instead of kick

--- lib/modules/CommandKick.class.php
+++ lib/modules/CommandKick.class.php
@@ -33,7 +33,7 @@ class CommandKick extends CommandModule {
 
 		if (count($messageEx) == 2) {
 			if ($access < $this->bot->getAccess($target, Services::getUserManager()->getUser($messageEx[1])->accountname))
-				Services::getConnection()->getProtocol()->sendKick($this->bot->getUuid(), $target, $user->getUuid(), $user->getNick()); // selfkick
+				return $this->bot->sendMessage($user->getUuid(), Services::getLanguage()->get($user->languageID, 'command.permissionDenied'));
 			else
 				Services::getConnection()->getProtocol()->sendKick($this->bot->getUuid(), $target, $messageEx[1], $user->getNick());
 			$this->bot->sendMessage($user->getUuid(), Services::getLanguage()->get($user->languageID, 'command.'.$this->originalName.'.success'));
@@ -43,7 +43,7 @@ class CommandKick extends CommandModule {
 			$username = $messageEx[1];
 			unset($messageEx[1]);
 			if ($access < $this->bot->getAccess($target, Services::getUserManager()->getUser($username)->accountname))
-				Services::getConnection()->getProtocol()->sendKick($this->bot->getUuid(), $target, $user->getUuid(), $user->getNick().': '.implode(' ', $messageEx)); // selfkick
+				return $this->bot->sendMessage($user->getUuid(), Services::getLanguage()->get($user->languageID, 'command.permissionDenied'));
 			else
 				Services::getConnection()->getProtocol()->sendKick($this->bot->getUuid(), $target, $username, $user->getNick().': '.implode(' ', $messageEx));
 			$this->bot->sendMessage($user->getUuid(), Services::getLanguage()->get($user->languageID, 'command.'.$this->originalName.'.success'));
