d793c52057274c4663a8d1a45c5c4b7b361ad72e
Johannes Donath <akkarin@develfusion.com>
2010-12-27 10:37:48 +0100

    Corrected className

--- lib/modules/OpServ.class.php
+++ lib/modules/OpServ.class.php
@@ -6,7 +6,7 @@ require_once(SDIR.'lib/modules/BotModule.class.php');
  * @author		Johannes Donath
  * @copyright	2010 DEVel Fusion
  */
-class OpServModule extends BotModule {
-	
+class OpServ extends BotModule {
+
 }
 ?>
