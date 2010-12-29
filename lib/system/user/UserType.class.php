889d31b04fce946f9c521cc89bcabacd1e2753d3
Johannes Donath <akkarin@develfusion.com>
2010-12-29 21:26:50 +0100

    Created UserType

---
+++ lib/system/user/UserType.class.php
@@ -0,0 +1,31 @@
+<?php
+
+/**
+ * Defines default methods for user types
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+interface UserType {
+	
+	/**
+	 * Creates a new instance of UserType
+	 * @param	mixed	$userID
+	 * @param	array	$data
+	 */
+	public function __construct($userID, $data);
+	
+	/**
+	 * Sets a user property
+	 * @param	string	$property
+	 * @param	mixed	$value
+	 */
+	public function __set($property, $value);
+	
+	/**
+	 * Gets a user property
+	 * @param	string	$property
+	 */
+	public function __get($property);
+}
+?>
