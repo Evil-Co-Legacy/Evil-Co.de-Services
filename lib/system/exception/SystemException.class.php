e526ad3917786f456df8974a569a7f4172bd21bf
Johannes Donath <akkarin@develfusion.com>
2010-12-29 18:30:18 +0100

    Added debug log to exception

--- lib/system/exception/SystemException.class.php
+++ lib/system/exception/SystemException.class.php
@@ -8,5 +8,30 @@
  */
 class SystemException extends Exception {
 	
+	/**
+	 * Sends a debug log with exception data
+	 */
+	public function sendDebugLog() {
+		// send message
+		$message = explode("\n", $this->message);
+		Services::getConnection()->sendLogLine("Exception thrown with message:".$message[0]);
+		
+		if (count($message) > 1) {
+			foreach($message as $key => $log) {
+				if ($key > 0) Services::getConnection()->sendLogLine($log);
+			}
+		}
+		
+		// send code
+		Services::getConnection()->sendLogLine("Code: ".$this->code);
+		
+		// send stacktrace
+		$stacktraceArray = explode("\n", $this->getTraceAsString());
+		
+		Services::getConnection()->sendLogLine("Stacktrace:");
+		foreach($stacktraceArray as $stacktrace) {
+			Services::getConnection()->sendLogLine($stacktrace);
+		}
+	}
 }
 ?>
