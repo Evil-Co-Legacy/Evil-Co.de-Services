541da1e473a19da89ebe88ee98b06a8c1721b301
Johannes Donath <akkarin@develfusion.com>
2010-12-29 22:02:15 +0100

    Created ConnectionException

---
+++ lib/system/exception/ConnectionException.class.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * This exception will thrown on connection problems
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+class ConnectionException extends SystemException { }
+?>
