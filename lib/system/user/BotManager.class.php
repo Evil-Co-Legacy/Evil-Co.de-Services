50c6f16528a551e5c9ee990fb74aac9083845552
Johannes Donath <akkarin@develfusion.com>
2010-12-29 23:10:21 +0100

    Created BotManager

---
+++ lib/system/user/BotManager.class.php
@@ -0,0 +1,19 @@
+<?php
+// imports
+require_once(SDIR.'lib/system/user/AbstractUserTypeManager.class.php');
+require_once(SDIR.'lib/system/user/BotUserType.class.php');
+
+/**
+ * Manages all service bots
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+class BotManager extends AbstractUserTypeManager {
+	
+	/**
+	 * @see AbstractUserTypeManager::$userType
+	 */
+	protected $userType = 'BotUserType';
+}
+?>
