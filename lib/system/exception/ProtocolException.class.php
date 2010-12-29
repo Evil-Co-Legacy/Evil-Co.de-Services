3fa60332789e1a77aa0ada2987c10d94f72ef8be
Johannes Donath <akkarin@develfusion.com>
2010-12-29 22:03:34 +0100

    Created ProtocolException

---
+++ lib/system/exception/ProtocolException.class.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * This exception will thrown on protocol missmatches and other scary things (This will never thrown ...)
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+class ProtocolException extends SystemException { }
+?>
