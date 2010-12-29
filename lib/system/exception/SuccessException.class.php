2b99ea4e7d85e65ea232f399c1d87b9e98b9bb45
Johannes Donath <akkarin@develfusion.com>
2010-12-29 21:41:58 +0100

    Corrected author (TimWolla was it!!!)

--- lib/system/exception/SuccessException.class.php
+++ lib/system/exception/SuccessException.class.php
@@ -2,7 +2,7 @@
 /**
  * This one should never be thrown!
  *
- * @author		Johannes Donath
+ * @author		Tim Düsterhus
  * @copyright	2010 DEVel Fusion
  * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  */
