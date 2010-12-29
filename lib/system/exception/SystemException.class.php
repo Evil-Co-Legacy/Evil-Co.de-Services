21570b394b31e9732202b834fc9246fbd2abd4dc
Johannes Donath <akkarin@develfusion.com>
2010-12-29 18:15:55 +0100

    Created SystemException

---
+++ lib/system/exception/SystemException.class.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * Exception for system errors
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+class SystemException extends Exception {
+	
+}
+?>
