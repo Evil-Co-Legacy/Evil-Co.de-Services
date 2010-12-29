0d1714e54085c16ab187213882c47a70246415ab
Johannes Donath <akkarin@develfusion.com>
2010-12-29 21:00:38 +0100

    Created AbstractUserTypeManager

---
+++ lib/system/user/AbstractUserTypeManager.class.php
@@ -0,0 +1,107 @@
+<?php
+// imports
+require_once(SDIR.'lib/system/user/AbstractUserType.class.php');
+require_once(SDIR.'lib/system/user/AbstractUserTypeManager.class.php');
+
+/**
+ * Manages a special type of users
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+abstract class AbstractUserTypeManager implements UserTypeManager, Iterator {
+	
+	/**
+	 * Contains all users to manage
+	 * @var array<UserType>
+	 */
+	protected $userList = array();
+	
+	/**
+	 * Contains the current iterator pointer
+	 * @var integer
+	 */
+	protected $userListPointer = 0;
+
+	/**
+	 * Contains the class name that should used for every user
+	 * @var string
+	 */
+	protected $userType = 'AbstractUserType';
+
+	/**
+	 * Adds a new user to userlist
+	 * @param	mixed			$userID
+	 * @param	array<mixed>	$data
+	 * @return void
+	 */
+	public function addUser($userID, $data = array()) {
+		$this->userList[$userID] = new $this->userType($userID, $data);
+	}
+	
+	/**
+	 * Returnes the user with ID $userID
+	 * @param	mixed	$userID
+	 * @return UserType
+	 */
+	public function getUser($userID) {
+		if (isset($this->userList[$userID])) return $this->userList[$userID];
+		return null;
+	}
+	
+	/**
+	 * Removes a user from userlist
+	 * @param	mixed	$userID
+	 * @return void
+	 */
+	public function removeUser($userID) {
+		if (isset($this->userList[$userID])) unset($this->userList[$userID]);
+	}
+	
+	// ITERATOR METHODS
+	/**
+	 * @see Iterator::rewind()
+	 */
+	public function rewind() {
+		$this->userListPointer--;
+	}
+	
+	/**
+	 * @see Iterator::current()
+	 */
+	public function current() {
+		// return value
+		return $this->getUser($this->key());
+	}
+	
+	/**
+	 * @see Ikarus::key()
+	 */
+	public function key() {
+		// get keys
+		$keys = array_keys($this->userList);
+		
+		// validate current pointer
+		if (!isset($keys[$this->userListPointer])) throw new SystemException("Pointer out of index");
+		
+		return $keys[$this->userListPointer];
+	}
+	
+	/**
+	 * @see Ikarus::next()
+	 */
+	public function next() {
+		$this->userListPointer++;
+	}
+	
+	/**
+	 * @see Ikarus::valid()
+	 */
+	public function valid() {
+		// get keys
+		$keys = array_keys($this->userList);
+		
+		return (isset($keys[$this->userListPointer]));
+	}
+}
+?>
