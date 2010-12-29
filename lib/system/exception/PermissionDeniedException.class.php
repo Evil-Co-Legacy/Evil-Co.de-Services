27b45ef9c341bb635a7be1a5cb7dda9731a3ec00
Johannes Donath <akkarin@develfusion.com>
2010-12-29 18:19:44 +0100

    Created PermissionDeniedException

---
+++ lib/system/exception/PermissionDeniedException.class.php
@@ -0,0 +1,12 @@
+<?php
+
+/**
+ * This exception will thrown if a user haven't the correct exceptions to use a command
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+class PermissionDeniedException extends UserException {
+	
+}
+?>
