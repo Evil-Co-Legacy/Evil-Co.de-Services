34e3d9658b24d717aa88725cee7619f9c3538acc
TimWolla <timwolla@arcor.de>
2010-12-29 21:08:45 +0100

    There was conflict content

--- lib/system/exception/PermissionDeniedException.class.php
+++ lib/system/exception/PermissionDeniedException.class.php
@@ -1,4 +1,3 @@
-<<<<<<< HEAD:lib/system/exception/PermissionDeniedException.class.php
 <?php
 
 /**
@@ -10,14 +9,4 @@
 class PermissionDeniedException extends UserException {
 	
 }
-=======
-<?php
-
-/**
- * Defines a special exception that can thrown in ModuleManager and modules
- * @author		Johannes Donath
- * @copyright	2010 DEVel Fusion
- */
-class ModuleException extends Exception { }
->>>>>>> parent of d133705... Added license information:lib/system/module/ModuleException.class.php
 ?>
