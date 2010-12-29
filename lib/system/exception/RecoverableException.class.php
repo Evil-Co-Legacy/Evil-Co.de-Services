a9e554b1a6aacabbf2506b49612fc0f3785ee532
Johannes Donath <akkarin@develfusion.com>
2010-12-29 21:40:32 +0100

    Created RecoverableException

---
+++ lib/system/exception/RecoverableException.class.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * This exception will thrown if an exception isn't fatal
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+class RecoverableException extends SystemException { }
+?>
