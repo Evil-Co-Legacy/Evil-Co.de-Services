b4cd61114e3c4cfd5fbeccf7f882fcef00352c92
TimWolla <timwolla@arcor.de>
2010-12-30 15:34:02 +0100

    More corrections

--- lib/modules/ExtensionModule.class.php
+++ lib/modules/ExtensionModule.class.php
@@ -4,8 +4,10 @@ require_once(SDIR.'lib/modules/AbstractModule.class.php');
 
 /**
  * Defines default methods for extensions
- * @author		Johannes Donath
+ *
+ * @author	Johannes Donath
  * @copyright	2010 DEVel Fusion
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  */
 abstract class ExtensionModule extends AbstractModule { }
 ?>
