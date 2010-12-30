c197365547bf9abf09fc5ae36f444b1ab8dd040a
Johannes Donath <akkarin@develfusion.com>
2010-12-30 01:31:18 +0100

    Created ModeList

---
+++ lib/system/irc/ModeList.class.php
@@ -0,0 +1,34 @@
+<?php
+
+/**
+ * Manages and parses modestrings
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+interface ModeList {
+	
+	/**
+	 * Creates a new instance of ModeList
+	 * @param	string	$modeString
+	 */
+	public function __construct($modeString);
+	
+	/**
+	 * Returnes true if the specified mode is set
+	 * @param	string(1)	$modeChar
+	 */
+	public function hasMode($modeChar);
+	
+	/**
+	 * Updates the current mode string
+	 * @param	string	$modeString
+	 */
+	public function updateModes($modeString);
+	
+	/**
+	 * Converts the ModeList to type string
+	 */
+	public function __toString();
+}
+?>
