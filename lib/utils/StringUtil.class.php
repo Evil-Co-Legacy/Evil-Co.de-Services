8f14929624425e0df718a117a09676ca160d3415
TimWolla <timwolla@arcor.de>
2010-12-30 15:25:37 +0100

    Corrected PHPDoc

--- lib/utils/StringUtil.class.php
+++ lib/utils/StringUtil.class.php
@@ -2,9 +2,9 @@
 /**
  * Contains string-related functions.
  * 
- * @author 		Marcel Werk
+ * @author 	Marcel Werk
  * @copyright	2001-2009 WoltLab GmbH
- * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  */
 class StringUtil {
 	const HTML_PATTERN = '~</?[a-z]+[1-6]?
--- start.php
+++ start.php
@@ -1,4 +1,12 @@
 <?php
+/**
+ * Starts the services
+ *
+ * @author	Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+
 // defines
 define('SDIR', dirname(__FILE__).'/');
 
