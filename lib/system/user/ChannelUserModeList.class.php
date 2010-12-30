9053967003039a5cc0032c96ccf4a9fe9b0da093
Johannes Donath <akkarin@develfusion.com>
2010-12-30 15:51:09 +0100

    Created ChannelUserModeList

---
+++ lib/system/user/ChannelUserModeList.class.php
@@ -0,0 +1,18 @@
+<?php
+// imports
+require_once(SDIR.'lib/system/irc/AbstractModeList.class.php');
+
+/**
+ * Manages modes for users in channels
+ * @author		Johannes Donath
+ * @copyright		2010 DEVel Fusion
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+class ChannelUserModeList extends AbstractModeList {
+	
+	/**
+	 * @see AbstractModeList::$modeInformationFilename
+	 */
+	protected static $modeInformationFilename = 'channelUserModes';
+}
+?>
