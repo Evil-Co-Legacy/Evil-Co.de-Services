4844a302f38070c3677be644efae98b3063548e0
Johannes Donath <akkarin@develfusion.com>
2010-12-30 13:45:41 +0100

    Created ArgumentList

---
+++ lib/system/irc/ArgumentList.class.php
@@ -0,0 +1,29 @@
+<?php
+
+/**
+ * Defines default methods for argument lists
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+interface ArgumentList {
+	
+	/**
+	 * Creates a new instance of type ArgumentList
+	 * @param	string	$argumentString
+	 */
+	public function __construct($argumentString);
+	
+	/**
+	 * Parses an argument list
+	 * @param	string	$argumentString
+	 */
+	protected function parseArgumentString($argumentString);
+	
+	/**
+	 * Returnes the argument for given index
+	 * @param	integer	$index
+	 */
+	public function getArgument($index);
+}
+?>
