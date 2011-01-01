482ed02c65c839e417e53296cd996e0a5989ae14
TimWolla <timwolla@arcor.de>
2011-01-01 01:05:56 +0100

    More PHPDoc

--- lib/system/irc/ArgumentList.class.php
+++ lib/system/irc/ArgumentList.class.php
@@ -1,21 +1,24 @@
 <?php
-
 /**
  * Defines default methods for argument lists
- * @author		Johannes Donath
+ *
+ * @author	Johannes Donath
  * @copyright	2010 DEVel Fusion
- * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ * @license	GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  */
 interface ArgumentList {
 	
 	/**
 	 * Creates a new instance of type ArgumentList
+	 *
+	 * @param	string	$modesource
 	 * @param	string	$argumentString
 	 */
 	public function __construct($modeSource, $argumentString);
 	
 	/**
 	 * Returnes the argument for given index
+	 *
 	 * @param	integer	$index
 	 */
 	public function getArgument($index);
