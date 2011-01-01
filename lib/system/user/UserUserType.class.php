c549af8cb74197f78c56c5b5f402dc936d33adaf
Johannes Donath <akkarin@develfusion.com>
2011-01-01 17:26:28 +0100

    Created UserUserType (Fucking name ...)

---
+++ lib/system/user/UserUserType.class.php
@@ -0,0 +1,12 @@
+<?php
+// imports
+require_once(SDIR.'lib/system/user/AbstractUserType.class.php');
+
+/**
+ * Represents a user
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+class UserUserType extends AbstractUserType { }
+?>
