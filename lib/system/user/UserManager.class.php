c53894adf1152a0502bc49814f6c681d049b4d0e
Johannes Donath <akkarin@develfusion.com>
2011-01-01 17:23:37 +0100

    Created UserManager

---
+++ lib/system/user/UserManager.class.php
@@ -0,0 +1,19 @@
+<?php
+// imports
+require_once(SDIR.'lib/system/user/AbstractUserTypeManager.class.php');
+require_once(SDIR.'lib/system/user/UserUserType.class.php');
+
+/**
+ * Manages all network users
+ * @author		Johannes Donath
+ * @copyright		2010 DEVel Fusion
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+class UserManager extends AbstractUserTypeManager {
+	
+	/**
+	 * @see AbstractUserTypeManager::$userType
+	 */
+	protected $userType = 'UserUserType';
+}
+?>
