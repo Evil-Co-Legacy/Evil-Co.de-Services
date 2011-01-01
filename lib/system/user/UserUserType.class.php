3a5c4e601e0cc9b03ec840046c054d14f53e4eb7
Johannes Donath <akkarin@develfusion.com>
2011-01-01 17:29:48 +0100

    PHPDoc

--- lib/system/user/UserUserType.class.php
+++ lib/system/user/UserUserType.class.php
@@ -5,7 +5,7 @@ require_once(SDIR.'lib/system/user/AbstractUserType.class.php');
 /**
  * Represents a user
  * @author		Johannes Donath
- * @copyright	2010 DEVel Fusion
+ * @copyright		2010 DEVel Fusion
  * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  */
 class UserUserType extends AbstractUserType { }
