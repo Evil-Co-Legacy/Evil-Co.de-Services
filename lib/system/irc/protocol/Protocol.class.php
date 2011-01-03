a7dca5ae0b1e7c8e10dee0d51961b2bb68c0300a
Johannes Donath <akkarin@develfusion.com>
2011-01-03 17:02:24 +0100

    Created Protocol

---
+++ lib/system/irc/protocol/Protocol.class.php
@@ -0,0 +1,21 @@
+<?php
+
+/**
+ * Defines default methods for protocols
+ * @author		Johannes Donath
+ * @copyright		2010 DEVel Fusion
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+interface Protocol {
+
+	/**
+	 * Creates a new instance of type Protocol
+	 */
+	public function __construct(&$supportedTypes);
+
+	/**
+	 * Returnes true if the current connection to server is alive
+	 */
+	public function isAlive();
+}
+?>
