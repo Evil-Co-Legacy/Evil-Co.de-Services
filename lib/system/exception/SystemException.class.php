cd6d3e863df37088b033c4cb49a99b634558c034
Johannes Donath <akkarin@develfusion.com>
2011-03-01 15:35:03 +0100

    Removed content of sendDebugLog method

--- lib/system/exception/SystemException.class.php
+++ lib/system/exception/SystemException.class.php
@@ -30,26 +30,7 @@ class SystemException extends Zend_Exception {
 	 * @return	void
 	 */
 	public function sendDebugLog() {
-		// send message
-		$message = explode("\n", $this->message);
-		Services::getConnection()->sendLogLine("Exception thrown with message:".$message[0]);
-		
-		if (count($message) > 1) {
-			foreach($message as $key => $log) {
-				if ($key > 0) Services::getConnection()->sendLogLine($log);
-			}
-		}
-		
-		// send code
-		Services::getConnection()->sendLogLine("Code: ".$this->code);
-		
-		// send stacktrace
-		$stacktraceArray = explode("\n", $this->__getTraceAsString());
-		
-		Services::getConnection()->sendLogLine("Stacktrace:");
-		foreach($stacktraceArray as $stacktrace) {
-			Services::getConnection()->sendLogLine($stacktrace);
-		}
+		// placeholder
 	}
 }
 ?>
