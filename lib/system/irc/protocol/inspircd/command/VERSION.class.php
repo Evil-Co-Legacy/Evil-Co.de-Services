a484bdca29b746bb9377b6d3c0541cda8d39df42
Johannes Donath <akkarin@develfusion.com>
2011-03-02 22:35:12 +0100

    Added version command parser

---
+++ lib/system/irc/protocol/inspircd/command/VERSION.class.php
@@ -0,0 +1,25 @@
+<?php
+// service imports
+require_once(SDIR.'lib/system/irc/protocol/CommandParser.class.php');
+
+/**
+ * Parses the VERSION command
+ * @author		Johannes Donath
+ * @copyright		2011 DEVel Fusion
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+class VERSION extends CommandParser {
+	
+	/**
+	 * @see CommandParser::parse()
+	 */
+	public function parse($line, $lineEx, $source = null) {
+		// get version string
+		$line = substr($line, 1);
+		$version = substr($line, (stripos($line, ':') + 1));
+		
+		// set version metadata
+		$source->version = $version;
+	}
+}
+?>
