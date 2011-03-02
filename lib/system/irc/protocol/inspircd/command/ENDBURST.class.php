994f23d567d00b28535856aafaecee506b6ebc66
Johannes Donath <akkarin@develfusion.com>
2011-03-02 18:27:13 +0100

    Created empty endburst command parser

---
+++ lib/system/irc/protocol/inspircd/command/ENDBURST.class.php
@@ -0,0 +1,18 @@
+<?php
+// service imports
+require_once(SDIR.'lib/system/irc/protocol/CommandParser.class.php');
+
+/**
+ * Parses the ENDBURST command
+ * @author		Johannes Donath
+ * @copyright		2011 DEVel Fusion
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+class ENDBURST extends CommandParser {
+	
+	/**
+	 * @see CommandParser::parse()
+	 */
+	public function parse($line, $lineEx) { }
+}
+?>
