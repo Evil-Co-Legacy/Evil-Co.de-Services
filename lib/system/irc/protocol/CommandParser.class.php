fbd0eb98e3b3442a9f9d93b74e52fbd939ac6493
Johannes Donath <akkarin@develfusion.com>
2011-03-02 15:51:12 +0100

    Removed factory pattern

--- lib/system/irc/protocol/CommandParser.class.php
+++ lib/system/irc/protocol/CommandParser.class.php
@@ -9,15 +9,9 @@
 abstract class CommandParser {
 	
 	/**
-	 * Contains an instance of CommandParser
-	 * @var CommandParser
-	 */
-	protected static $instance = null;
-	
-	/**
 	 * Creates a new instance of type CommandParser
 	 */
-	protected function __construct() { }
+	public function __construct() { }
 	
 	/**
 	 * Parses the given line
@@ -25,16 +19,5 @@ abstract class CommandParser {
 	 * @param	string	$lineEx
 	 */
 	abstract public function parse($line, $lineEx);
-	
-	/**
-	 * Returnes an instance of CommandParser
-	 */
-	public static final function getInstance() {
-		if (static::$instance === null) {
-			static::$instance = new static();
-		}
-		
-		return static::$instance;
-	}
 }
 ?>
