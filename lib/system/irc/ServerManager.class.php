e5b6884fb89bb874504badfeaf6fc476f9e517df
Johannes Donath <akkarin@develfusion.com>
2011-03-02 19:34:19 +0100

    Implemented ServerManager

---
+++ lib/system/irc/ServerManager.class.php
@@ -0,0 +1,106 @@
+<?php
+// service imports
+require_once(SDIR.'lib/system/irc/ConnectedServer.class.php');
+
+/**
+ * Manages servers
+ * @author		Johannes Donath
+ * @copyright		2011 DEVel Fusion
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+class ServerManager implements Iterator {
+	
+	/**
+	 * Contains all registered servers
+	 * @var array<ConnectedServer>
+	 */
+	protected $serverList = array();
+	
+	/**
+	 * Contains the pointer for iterator feature
+	 * @var integer
+	 */
+	protected $serverListPointer = 0;
+	
+	/**
+	 * @see Iterator::current()
+	 */
+	public function current() {
+		// get keys
+		$keys = array_keys($this->serverList);
+		
+		// get identifier
+		$identifier = $keys[$this->serverListPointer];
+		
+		// return ConnectedServer object
+		return $this->serverList[$identifier];
+	}
+	
+	/**
+	 * @see Iterator::key()
+	 */
+	public function key() {
+		// get keys
+		$keys = array_keys($this->serverList);
+		
+		// return current keys
+		return $keys[$this->serverListPointer];
+	}
+	
+	/**
+	 * @see Iterator::next()
+	 */
+	public function next() {
+		++$this->serverListPointer;
+	}
+	
+	/**
+	 * Registeres a new server
+	 * @param	string	$serverName
+	 * @param	mixed	$identifier
+	 * @return mixed
+	 * @throws RecoverableException
+	 */
+	public function registerServer($serverName, $identifier) {
+		// validate
+		if (isset($this->serverList[$identifier])) throw new RecoverableException("A server with identifier '".$identifier."' is already registered");
+		
+		// add to list
+		$this->serverList[$identifier] = new ConnectedServer($serverName, $identifier);
+		
+		// return identifier
+		return $identifier;
+	}
+	
+	/**
+	 * @see Iterator::rewind()
+	 */
+	public function rewind() {
+		$this->serverListPointer = 0;
+	}
+	
+	/**
+	 * Removes a server
+	 * @param	mixed	$identifier
+	 * @return void
+	 * @throws RecoverableException
+	 */
+	public function unregisterServer($identifier) {
+		// validate
+		if (!isset($this->serverList[$identifier])) throw new RecoverableException("Tried to unregister unknown server '".$identifier."'");
+		
+		// unregister
+		unset($this->serverList[$identifier]);
+	}
+	
+	/**
+	 * @see Iterator::valid()
+	 */
+	public function valid() {
+		// get keys
+		$keys = array_keys($this->serverList);
+		
+		return (isset($keys[$this->serverListPointer]));
+	}
+}
+?>
