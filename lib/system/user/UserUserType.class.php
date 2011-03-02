f6739fc62b67cc0a7d4b84f3e7b20d747dad8707
Johannes Donath <akkarin@develfusion.com>
2011-03-03 00:26:33 +0100

    Added propertyModified event (e.g. usefull for inspircd's metadata implementation)

--- lib/system/user/UserUserType.class.php
+++ lib/system/user/UserUserType.class.php
@@ -9,5 +9,16 @@ require_once(SDIR.'lib/system/user/AbstractUserType.class.php');
  * @copyright		2010 DEVel Fusion
  * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  */
-class UserUserType extends AbstractUserType { }
+class UserUserType extends AbstractUserType {
+	
+	/**
+	 * @see AbstractUserType::__set()
+	 */
+	public function __set($property, $value) {
+		parent::__set($property, $value);
+		
+		// fire event
+		Services::getEvent()->fire($this, 'propertyModified', array('property' => $property, 'value' => $value));
+	}
+}
 ?>
