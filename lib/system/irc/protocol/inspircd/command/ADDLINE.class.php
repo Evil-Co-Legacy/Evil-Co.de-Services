1b6a13c59c7b0051a12ac3f35bc7f26b8db3a2d2
Johannes Donath <akkarin@develfusion.com>
2011-03-02 21:09:11 +0100

    Added ADDLINE parser

---
+++ lib/system/irc/protocol/inspircd/command/ADDLINE.class.php
@@ -0,0 +1,27 @@
+<?php
+// service imports
+require_once(SDIR.'lib/system/irc/protocol/CommandParser.class.php');
+
+/**
+ * Parses the ADDLINE command
+ * @author		Johannes Donath
+ * @copyright		2011 DEVel Fusion
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+class ADDLINE extends CommandParser {
+	
+	/**
+	 * @see CommandParser::parse()
+	 */
+	public function parse($line, $lineEx, $source = null) {
+		// remove numeric
+		$line = substr($line, 4);
+		
+		// get reason
+		$reason = substr($line, (stripos($line, ':') + 1));
+		
+		// add line
+		Services::getLineManager()->addLine($lineEx[1], $lineEx[2], $lineEx[3], $lineEx[4], $lineEx[5], $reason);
+	}
+}
+?>
