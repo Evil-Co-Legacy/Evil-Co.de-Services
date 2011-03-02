fac44f0c717b4cdaf3c46e2b59ed1949b7b028bf
Johannes Donath <akkarin@develfusion.com>
2011-03-02 03:58:00 +0100

    Several corrections, fixes and workarounds

--- lib/system/log/IRCLogWriter.class.php
+++ lib/system/log/IRCLogWriter.class.php
@@ -34,6 +34,7 @@ class IRCLogWriter extends Zend_Log_Writer_Abstract {
 	 * @return void
 	 */
 	protected function _write($event) {
+		// FIXME: This sends infinite lines of nonsense to server ..
 		if (Services::getConnection() !== null and Services::getConnection()->isAlive() and Services::getProtocol() !== null and Services::getProtocol()->isAlive() and Services::getProtocol()->isReady()) {
 			// get log string
 			$line = $this->_formatter->format($event);
@@ -41,14 +42,25 @@ class IRCLogWriter extends Zend_Log_Writer_Abstract {
 			// trim message
 			$line = StringUtil::trim($line);
 			
+			// unify newlines
+			$line = StringUtil::unifyNewlines($line);
+			
+			// remove tabs
+			$line = str_replace("\t", "        ", $line);
+			
 			// detect newlines
 			if (stripos($line, "\n") !== null) {
 				// split
 				$lineEx = explode("\n", $line);
 				
 				// send each line to service channel
-				foreach($lineEx as $line) {
-					Services::getProtocol()->sendMessage(Services::getConfiguration()->connection->servicechannel, $line);
+				foreach($lineEx as $newline) {
+					var_dump($newline);
+					// trim message
+					$newline = StringUtil::trim($newline);
+					
+					// send
+					if (!empty($newline)) Services::getProtocol()->sendMessage(Services::getConfiguration()->connection->servicechannel, $newline);
 				}
 			} else {
 				Services::getProtocol()->sendMessage(Services::getConfiguration()->connection->servicechannel, $line);
