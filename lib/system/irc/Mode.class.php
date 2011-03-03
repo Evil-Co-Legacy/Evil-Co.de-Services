ea0b979d8f1a0af75175846ca67f4125b79e9d3d
Johannes Donath <akkarin@develfusion.com>
2011-03-03 19:15:26 +0100

    Added missing Mode class

---
+++ lib/system/irc/Mode.class.php
@@ -0,0 +1,49 @@
+<?php
+
+/**
+ * Represents a mode
+ * @author		Johannes Donath
+ * @copyright		2011 DEVel Fusion
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+class Mode {
+	
+	/**
+	 * Contains the mode char of this mode
+	 * @var string(1)
+	 */
+	protected $modeChar = '';
+	
+	/**
+	 * Contains the argument for this mode
+	 * @var string
+	 */
+	protected $argument = '';
+	
+	/**
+	 * Creates a new instance of type Mode
+	 * @param	string	$char
+	 * @param	string	$argument
+	 */
+	public function _construct($char, $argument = null) {
+		$this->modeChar = $char;
+		if ($argument !== null) $this->argument = $argument;
+	}
+	
+	/**
+	 * Returnes the argument of this mode
+	 * @var string
+	 */
+	public function getArgument() {
+		return $this->argument;
+	}
+	
+	/**
+	 * Converts this mode to string
+	 * @var string
+	 */
+	public function __toString() {
+		return $this->modeChar;
+	}
+}
+?>
