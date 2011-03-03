dacd85cb0308bc0bf4c44742033fb8eeeefd92ab
Johannes Donath <akkarin@develfusion.com>
2011-03-03 03:33:04 +0100

    Implemented parser for error command

---
+++ lib/system/irc/protocol/inspircd/command/ERROR.class.php
@@ -0,0 +1,27 @@
+<?php
+// service imports
+require_once(SDIR.'lib/system/irc/protocol/CommandParser.class.php');
+
+/**
+ * Parses the ERROR command
+ * @author		Johannes Donath
+ * @copyright		2011 DEVel Fusion
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+class ERROR extends CommandParser {
+	
+	/**
+	 * @see CommandParser::parse()
+	 */
+	public function parse($line, $lineEx, $source = null) {
+		// remove : prefix
+		$line = substr($line, 1);
+		
+		// send log line with error message
+		Services::getLog()->err("Received error from server: ".substr($line, (stripos($line, ':') + 1)));
+		
+		// shut down services
+		exit;
+	}
+}
+?>
