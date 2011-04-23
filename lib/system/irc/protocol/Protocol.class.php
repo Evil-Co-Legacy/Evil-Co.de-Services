e7ffb3b0c4e33cf687b460523b5a06de6f4b1d59
Johannes Donath <akkarin@develfusion.com>
2011-04-23 03:25:09 +0200

    Switched back to interface

--- lib/system/irc/protocol/Protocol.class.php
+++ lib/system/irc/protocol/Protocol.class.php
@@ -6,42 +6,42 @@
  * @copyright		2010 DEVel Fusion
  * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  */
-abstract class Protocol {
+interface Protocol {
 
 	/**
 	 * Creates a new instance of type Protocol
 	 */
-	abstract public function __construct(&$supportedTypes);
+	public function __construct(&$supportedTypes);
 	
 	/**
 	 * Handles exceptions in protocol system
 	 * @param	ProtocolException	$ex
 	 * @return void
 	 */
-	abstract public function handleException(ProtocolException $ex);
+	public function handleException(ProtocolException $ex);
 
 	/**
 	 * Inits the IRC server-to-server connection
 	 * @return void
 	 */
-	abstract public function initConnection();
+	public function initConnection();
 	
 	/**
 	 * Returnes true if the current connection to server is alive
 	 * @return boolean
 	 */
-	abstract public function isAlive();
+	public function isAlive();
 	
 	/**
 	 * Returnes true if the current connection to server is ready for msgs and other funny things
 	 * @return boolean
 	 */
-	abstract public function isReady();
+	public function isReady();
 	
 	/**
 	 * Shuts the connection down
 	 * @return void
 	 */
-	abstract public function shutdown();
+	public function shutdown();
 }
 ?>
