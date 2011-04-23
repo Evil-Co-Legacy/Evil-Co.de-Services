3f2c0e1438c00dd1218c5897c636e1e0695ebb43
Johannes Donath <akkarin@develfusion.com>
2011-04-23 03:17:01 +0200

    We've to use an abstract class here. PHP will not show interfaces as parent

--- lib/system/irc/protocol/Protocol.class.php
+++ lib/system/irc/protocol/Protocol.class.php
@@ -6,42 +6,42 @@
  * @copyright		2010 DEVel Fusion
  * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  */
-interface Protocol {
+abstract class Protocol {
 
 	/**
 	 * Creates a new instance of type Protocol
 	 */
-	public function __construct(&$supportedTypes);
+	abstract public function __construct(&$supportedTypes);
 	
 	/**
 	 * Handles exceptions in protocol system
 	 * @param	ProtocolException	$ex
 	 * @return void
 	 */
-	public function handleException(ProtocolException $ex);
+	abstract public function handleException(ProtocolException $ex);
 
 	/**
 	 * Inits the IRC server-to-server connection
 	 * @return void
 	 */
-	public function initConnection();
+	abstract public function initConnection();
 	
 	/**
 	 * Returnes true if the current connection to server is alive
 	 * @return boolean
 	 */
-	public function isAlive();
+	abstract public function isAlive();
 	
 	/**
 	 * Returnes true if the current connection to server is ready for msgs and other funny things
 	 * @return boolean
 	 */
-	public function isReady();
+	abstract public function isReady();
 	
 	/**
 	 * Shuts the connection down
 	 * @return void
 	 */
-	public function shutdown();
+	abstract public function shutdown();
 }
 ?>
