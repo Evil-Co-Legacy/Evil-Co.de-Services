bfc5c6b0538b4f9eee4efef8ce50318c06a22749
Johannes Donath <akkarin@develfusion.com>
2011-05-01 14:28:47 +0200

    Created senseless DevNull class

---
+++ lib/utils/DevNull.class.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Does nothing with sense
+ * @author		Johannes Donath
+ * @copyright		2011 DEVel Fusion
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+class DevNull {
+	
+	/**
+	 * Returnes the answer of life, the universe and everything
+	 * @return integer
+	 */
+	public function getTheAnswerOfLifeTheUniverseAndEverything() {
+		return 42;
+	}
+}
+?>
