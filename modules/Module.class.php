91f835d2dc385dd2afb7f1e33f5da8603eefabaf
Johannes Donath <akkarin@develfusion.com>
2011-05-01 17:43:28 +0200

    WUHAHAHAHAHAHAHAHA!

---
+++ modules/Module.class.php
@@ -0,0 +1,17 @@
+<?php
+
+/**
+ * Defines default methods that must be implemented in each module type
+ * @author		Johannes Donath
+ * @copyright		2011 DEVel Fusion
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+interface Module {
+	
+	/**
+	 * YEP! HE IS!
+	 * @return RETURNS THE FUCK TRUE!!!!
+	 */
+	public function timIsSilly();
+}
+?>
