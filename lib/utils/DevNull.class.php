06b279d6a52b1c5decdde5dce5cd117e3a037689
TimWolla <timwolla@arcor.de>
2011-05-01 15:06:29 +0200

    Its even more senseless now

--- lib/utils/DevNull.class.php
+++ lib/utils/DevNull.class.php
@@ -1,19 +1,21 @@
 <?php
-
 /**
  * Does nothing with sense
- * @author		Johannes Donath
+ *
+ * @author		Johannes Donath, Tim Düsterhus
  * @copyright		2011 DEVel Fusion
  * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
  */
 class DevNull {
+	const HALF_ANSWER = 21;
 	
 	/**
 	 * Returns the answer of life, the universe and everything
+	 *
 	 * @return integer
 	 */
 	public function getTheAnswerOfLifeTheUniverseAndEverything() {
-		return 42;
+		return self::HALF_ANSWER * 2;
 	}
 }
 ?>
