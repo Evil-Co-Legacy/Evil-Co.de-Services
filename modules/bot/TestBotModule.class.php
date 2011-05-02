1c24ad33eb8d2a214ec29573b855156826fc9384
Johannes Donath <akkarin@develfusion.com>
2011-05-02 17:47:41 +0200

    Ok ... Let's make some sensefull commits. But ... is this class really not senseless? o.O

---
+++ modules/bot/TestBotModule.class.php
@@ -0,0 +1,12 @@
+<?php
+// services imports
+require_once(SDIR.'modules/bot/BotModule.class.php');
+
+/**
+ * This class implements a senseless bot that does nothing  ...
+ * @author		Johannes Donath
+ * @copyright		2011 DEVel Fusion
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+class TestBotModule extends BotModule implements SenselessInterfaceOne, SenselessInterfaceTwo { }
+?>
