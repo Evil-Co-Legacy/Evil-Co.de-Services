74f86b4c574fc8833dd37f75cdaf76162d9dc1f9
Johannes Donath <akkarin@develfusion.com>
2011-05-02 17:39:29 +0200

    W T F?!

---
+++ lib/utils/SenselessInterfaceOne.class.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Just one of two senseless interfaces ...
+ * @author		Johannes Donath
+ * @copyright		2011 DEVel Fusion
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+interface SenselessInterfaceOne { };
+?>
---
+++ lib/utils/SenselessInterfaceTwo.class.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * Just the second of two senseless interfaces ...
+ * @author		Johannes Donath
+ * @copyright		2011 DEVel Fusion
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+interface SenselessInterfaceTwo { }
+?>
