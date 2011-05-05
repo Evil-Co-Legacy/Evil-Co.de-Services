589791d94746ba2eb47e0404e7bd82e2ac30a25a
Johannes Donath <akkarin@develfusion.com>
2011-05-05 23:17:07 +0200

    Nothing to do here at this moment

---
+++ modules/bot/BotModule.class.php
@@ -0,0 +1,12 @@
+<?php
+// service imports
+require_once(SDIR.'modules/extension/ExtensionModule.class.php');
+
+/**
+ * Defines default methods for bots
+ * @author		Johannes Donath
+ * @copyright		2011 DEVel Fusion
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+abstract class BotModule extends ExtensionModule { }
+?>
