9482d7a829c7f1232cedcc0bd5a6f3521eb0faa9
TimWolla <timwolla@arcor.de>
2011-05-06 21:05:48 +0200

    Fixed typo

--- lib/system/Services.class.php
+++ lib/system/Services.class.php
@@ -198,7 +198,7 @@ final class Services {
 		self::getLogger()->addWriter($inline);
 		
 		if (!DEBUG) {
-			$filter = new Zend_Log_Filter_Priority(min(0, max((Zeng_LOG::DEBUG - 1), Zend_Log::ERR - (int) self::getArguments()->quiet + (int) self::getArguments()->verbose)), '<=');
+			$filter = new Zend_Log_Filter_Priority(min(0, max((Zend_LOG::DEBUG - 1), Zend_Log::ERR - (int) self::getArguments()->quiet + (int) self::getArguments()->verbose)), '<=');
 			$inline->addFilter($filter);
 			$irc->addFilter($filter);
 			
