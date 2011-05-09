26877326512bfed7cec45cbea7d7865a1fbc3e3f
Johannes Donath <akkarin@develfusion.com>
2011-05-09 16:41:57 +0200

    BLAH!

--- lib/system/user/BotManager.class.php
+++ lib/system/user/BotManager.class.php
@@ -63,7 +63,7 @@ class BotManager extends AbstractUserTypeManager {
 			Services::getProtocolManager()->sendJoin($instance->getInstance()->uuid, Services::getConfiguration()->connection->servicechannel);
 			
 			// set op
-			Services::getProtocolManager()->sendMode($instance->getInstance()->uuid, Services::getConfiguration()->connection->servicechannel, '+o '.$instance->getInstance()->uuid);
+			Services::getProtocolManager()->userSendMode($instance->getInstance()->uuid, Services::getConfiguration()->connection->servicechannel, '+o '.$instance->getInstance()->nickname);
 		} else {
 			Services::getLogger()->info("No login information for Module found '".$instance->getModuleInformation()->moduleName."'");
 		}
