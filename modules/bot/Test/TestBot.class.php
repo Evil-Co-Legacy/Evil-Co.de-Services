10dbbfc4c78a7daf18cb92b4cb79e840c8266174
Johannes Donath <akkarin@develfusion.com>
2011-05-09 16:58:23 +0200

    Added Test module ...

---
+++ modules/bot/Test/TestBot.class.php
@@ -0,0 +1,10 @@
+<?php
+
+/**
+ * This implements a bot that does absolutly nothing!
+ * @author		Johannes Donath
+ * @copyright		2011 DEVel Fusion
+ * @license		GNU Lesser General Public License <http://opensource.org/licenses/lgpl-license.php>
+ */
+class TestBot extends BotModule { }
+?>
